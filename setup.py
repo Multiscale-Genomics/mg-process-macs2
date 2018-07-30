b2faaacc89de67a0251c84fb664b1344637c815a
Mark McDowall <mark.mcdowall@gmail.com>
2018-07-30 11:55:35 +0100

    Tidied up the installation so that only the non-pips libraries are in the requirements.txt

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'pytest'
+        'pytest', 'pylint', 'pysam', 'numpy', 'macs2', 'ConfigParser'
     ],
     setup_requires=[
         'pytest-runner',
