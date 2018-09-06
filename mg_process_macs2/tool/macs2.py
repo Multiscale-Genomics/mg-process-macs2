9653aca52d78dcca1134b67cc64cb2f779ba07f3
Mark McDowall <mark.mcdowall@gmail.com>
2018-09-06 13:16:10 +0100

    Removed the requirement for the file_path in the config.json file

--- mg_process_macs2/tool/macs2.py
+++ mg_process_macs2/tool/macs2.py
@@ -208,9 +208,8 @@ class Macs2(Tool):
             List of matching metadata dict objects
 
         """
-        root_name = input_files['bam'].split("/")
-        root_name[-1] = root_name[-1].replace('.bam', '')
-        name = root_name[-1]
+        root_name = os.path.split(input_files['bam'])
+        name = root_name[1].replace('.bam', '')
 
         # input and output share most metadata
         output_bed_types = {
@@ -219,6 +218,11 @@ class Macs2(Tool):
             'broad_peak': "bed6+3",
             'gapped_peak': "bed12+3"
         }
+
+        for k in output_bed_types:
+            if output_files[k] is None:
+                output_files[k] = os.path.join(
+                    self.configuration['execution'], name + "_" + k + ".bed")
 
         command_params = self.get_macs2_params(self.configuration)
 
