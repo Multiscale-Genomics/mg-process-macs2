15742f4a125f1201f1b31f739c9cb206d255332b
Mark McDowall <mark.mcdowall@gmail.com>
2018-09-07 14:33:50 +0100

    Better handling of chromosome names where the fasta header contained pipes

--- mg_process_macs2/tool/macs2.py
+++ mg_process_macs2/tool/macs2.py
@@ -420,6 +420,9 @@ class Macs2(Tool):
 
         logger.info("MACS2 COMMAND PARAMS: " + ", ".join(command_params))
 
+        for i in enumerate(chr_list):
+            chr_list[i] = chr_list[i].replace("|", "_")
+
         for chromosome in chr_list:
             if 'bam_bg' in input_files:
                 result = self.macs2_peak_calling(
