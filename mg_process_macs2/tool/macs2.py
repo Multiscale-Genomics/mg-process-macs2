1847af524ec7254eaae0fe14e256986ae2cca927
Mark McDowall <mark.mcdowall@gmail.com>
2018-09-07 14:36:34 +0100

    Error with handling tuple

--- mg_process_macs2/tool/macs2.py
+++ mg_process_macs2/tool/macs2.py
@@ -420,7 +420,7 @@ class Macs2(Tool):
 
         logger.info("MACS2 COMMAND PARAMS: " + ", ".join(command_params))
 
-        for i in enumerate(chr_list):
+        for i, j in enumerate(chr_list):
             chr_list[i] = chr_list[i].replace("|", "_")
 
         for chromosome in chr_list:
